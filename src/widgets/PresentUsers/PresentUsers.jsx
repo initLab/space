0451305c2a3fa9699dcf673e5f80db3afd023cff
Vencislav Atanasov <git@6bez10.info>
2022-05-07 23:02:16 +0300

    Add translation to PresentUsers

--- src/widgets/PresentUsers/PresentUsers.jsx
+++ src/widgets/PresentUsers/PresentUsers.jsx
@@ -1,15 +1,18 @@
 import {Col, Row} from "react-bootstrap";
 import PropTypes from "prop-types";
 import "./PresentUsers.scss";
+import {useTranslation} from "react-i18next";
 
 const PresentUsers = ({
     users,
 }) => {
+    const { t } = useTranslation();
+
     if (users.length === 0) {
         return (<Row>
             <Col className="no_users">
                 <i className="far fa-frown" />
-                <h5>everybodys_gone</h5>
+                <h5>{t('views.users.everybodys_gone')}</h5>
             </Col>
         </Row>);
     }
