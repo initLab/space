fee3b1ea39fd1d94bd9ed7e0fb55c503587684f2
Vencislav Atanasov <git@6bez10.info>
2022-05-07 23:03:19 +0300

    Fix Dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,17 +1,21 @@
 import {Col, Container, Row} from "react-bootstrap";
-import PresentUsers from "../widgets/PresentUsers";
+import PresentUsers from "../widgets/PresentUsers/PresentUsers";
+import {useTranslation} from "react-i18next";
+import {mqtt} from "../config";
+import SensorReading from "../widgets/SensorReading/SensorReading";
 
 const Dashboard = () => {
+    const { t } = useTranslation();
+
     return (<>
-        <div>whos_in_the_lab</div>
         <Container as="section">
             <Row as="header">
                 <Col sm={12}>
                     <h2>
-                        whos_in_the_lab
+                        {t('views.users.whos_in_the_lab')}
                         <div className="float-end">
                             <span className="small">
-                                people_at_about_html
+                                {t('views.users.people_at_about_html.few')}{' '}
                                 <time dateTime="2022-05-07T19:02:46+03:00">19:02</time>
                             </span>
                         </div>
@@ -21,15 +25,13 @@ const Dashboard = () => {
             <PresentUsers users={[]} />
             <Row>
                 <Col>
-                    <h3>sensor_readings</h3>
+                    <h3>{t('views.dashboard.sensor_readings')}</h3>
                 </Col>
             </Row>
             <Row>
-                {/* foreach */}
-                <Col sm={4}>
-                    <div data-label="label" data-label-type="Temperature" data-mqtt-topic="topic">Loading...</div>
-                </Col>
-                {/* endforeach */}
+                {mqtt.sensors.map((sensor, idx) =>
+                    <SensorReading key={idx} {...sensor} />
+                )}
             </Row>
         </Container>
     </>);
