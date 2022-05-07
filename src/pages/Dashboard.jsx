3b0e579d7ee76a34d73e987b04a9c847e29557a0
Vencislav Atanasov <git@6bez10.info>
2022-05-07 21:27:32 +0300

    Use react-bootstrap for the dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,44 +1,37 @@
+import {Col, Container, Row} from "react-bootstrap";
+import PresentUsers from "../widgets/PresentUsers";
+
 const Dashboard = () => {
     return (<>
         <div>whos_in_the_lab</div>
-        <section className="container">
-            <header className="row">
-                <div className="col-sm-12">
+        <Container as="section">
+            <Row as="header">
+                <Col sm={12}>
                     <h2>
                         whos_in_the_lab
-                        <div className="pull-right">
+                        <div className="float-end">
                             <span className="small">
                                 people_at_about_html
                                 <time dateTime="2022-05-07T19:02:46+03:00">19:02</time>
                             </span>
                         </div>
                     </h2>
-                </div>
-            </header>
-            <div className="present_users">
-                {/* if */}
-                <div className="row">
-                    present_user
-                </div>
-                {/* else */}
-                <div className="row">
-                    <div className="no_users">
-                        frown-o
-                        <h5>everybodys_gone
-                        </h5>
-                    </div>
-                </div>
-                {/* endif */}</div>
-            <h3>sensor_readings
-            </h3>
-            <div className="row">
+                </Col>
+            </Row>
+            <PresentUsers users={[]} />
+            <Row>
+                <Col>
+                    <h3>sensor_readings</h3>
+                </Col>
+            </Row>
+            <Row>
                 {/* foreach */}
-                <div className="col-sm-4">
-                    <div data-label="label" data-label-type="Temperature" data-mqtt-topic="topic">Loading...
-                    </div>
-                </div>
-                {/* endforeach */}</div>
-        </section>
+                <Col sm={4}>
+                    <div data-label="label" data-label-type="Temperature" data-mqtt-topic="topic">Loading...</div>
+                </Col>
+                {/* endforeach */}
+            </Row>
+        </Container>
     </>);
 };
 
