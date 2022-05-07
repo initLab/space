4c217f5d65dc0ce636bf0b01d48d3a6580508f9a
Vencislav Atanasov <git@6bez10.info>
2022-05-07 21:57:55 +0300

    Create i18n config and include it

---
+++ src/i18n.js
@@ -0,0 +1,20 @@
+import i18n from "i18next";
+import Backend from 'i18next-http-backend';
+import { initReactI18next } from "react-i18next";
+import { load } from 'js-yaml';
+
+i18n
+    .use(Backend)
+    .use(initReactI18next)
+    .init({
+        lng: 'en',
+        backend: {
+            loadPath: '/locales/{{lng}}.yaml',
+            parse: data => load(data),
+        },
+        interpolation: {
+            escapeValue: false,
+        },
+    });
+
+export default i18n;
