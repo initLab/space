804d1d2796eb420ad15a2c3484677ae874b11b83
Vencislav Atanasov <git@6bez10.info>
2022-05-07 23:02:54 +0300

    Add SensorReading component

---
+++ src/widgets/SensorReading/SensorReading.jsx
@@ -0,0 +1,33 @@
+import {Card, Col, Container, Row} from "react-bootstrap";
+import PropTypes from "prop-types";
+import './SensorReading/SensorReading.css';
+
+const SensorReading = ({
+    label,
+    topic,
+}) => {
+    return (<Col sm={4}>
+        <Card bg="primary" text="white">
+            <Card.Body>
+                <Container>
+                    <Row>
+                        <Col xs={3}>
+                            <i className="fa fa-thermometer-half fa-5x" />
+                        </Col>
+                        <Col xs={9} className="text-end">
+                            <div className="huge">21.9°C</div>
+                            <div>{label}</div>
+                        </Col>
+                    </Row>
+                </Container>
+            </Card.Body>
+        </Card>
+    </Col>);
+};
+
+SensorReading.propTypes = {
+    label: PropTypes.string.isRequired,
+    topic: PropTypes.string.isRequired,
+};
+
+export default SensorReading;
