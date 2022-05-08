e0a76c84291505b3015c635cb943f887445ab4cc
Vencislav Atanasov <git@6bez10.info>
2022-05-08 18:19:56 +0300

    Fix footer text alignment

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,14 +1,15 @@
 import {Col, Container, Row} from "react-bootstrap";
+import './Footer.scss';
 
 const Footer = () => {
-    return (<footer className="footer">
+    return (<footer>
         <Container>
             <Row>
                 <Col xs={12} sm={6}>
                     <p className="my-4">init Lab Fauna is licensed under <a href="https://github.com/initLab/fauna/blob/master/LICENSE">MIT License</a></p>
                 </Col>
                 <Col xs={12} sm={6}>
-                    <p className="my-4 text-end">The source code is available in <a
+                    <p className="my-4 right">The source code is available in <a
                         href="https://github.com/initLab/fauna/">GitHub</a></p>
                 </Col>
             </Row>
