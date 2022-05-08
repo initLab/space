6603481b3ca7d26e19d489f38fc41e0a454e384e
Vencislav Atanasov <git@6bez10.info>
2022-05-08 18:03:37 +0300

    Fix spacing

--- src/widgets/PresentUsers/PresentUsers.jsx
+++ src/widgets/PresentUsers/PresentUsers.jsx
@@ -15,7 +15,7 @@ const PresentUsers = () => {
         pollingInterval: 60000,
     });
 
-    return (<Row className={isSuccess && 'row-cols-1 row-cols-sm-2 row-cols-md-4'}>
+    return (<Row className={isSuccess && 'row-cols-1 row-cols-sm-2 row-cols-md-4 gy-3 mb-3'}>
         {isLoading && <Col>
             <LoadingIcon large/>
         </Col>}
@@ -24,29 +24,29 @@ const PresentUsers = () => {
                 <i className="far fa-frown"/>
                 <h5>{t('views.users.everybodys_gone')}</h5>
             </Col>
-        ) : users.map(user => <Col key={user.picture}>
+        ) : users.map(user => <Col key={user.picture} className="d-flex">
             <Card>
-                <Card.Body>
+                <Card.Header>
                     {user.picture &&
                         <Image fluid src={user.picture.replace('s=128', 's=242')} alt="avatar" className="w-100"/>}
-                    <div className="text-center">
-                        <h5>
-                            {user.name}
-                            {user.username && <div className="small">{user.username}</div>}
-                        </h5>
-                        <ButtonGroup>
-                            {user.twitter && <Button href={'https://twitter.com/' + user.twitter} target="_blank">
-                                <i className="fab fa-twitter fa-2x"></i>
+                </Card.Header>
+                <Card.Body className="text-center">
+                    <h5>
+                        {user.name}
+                        {user.username && <div className="small">{user.username}</div>}
+                    </h5>
+                    <ButtonGroup>
+                        {user.twitter && <Button href={'https://twitter.com/' + user.twitter} target="_blank">
+                            <i className="fab fa-twitter fa-2x"></i>
+                        </Button>}
+                        {user.github &&
+                            <Button variant="default" href={'https://github.com/' + user.github} target="_blank">
+                                <i className="fab fa-github fa-2x"></i>
                             </Button>}
-                            {user.github &&
-                                <Button variant="default" href={'https://github.com/' + user.github} target="_blank">
-                                    <i className="fab fa-github fa-2x"></i>
-                                </Button>}
-                            {user.url && <Button variant="info" href={user.url} target="_blank">
-                                <i className="fa fa-link fa-2x"></i>
-                            </Button>}
-                        </ButtonGroup>
-                    </div>
+                        {user.url && <Button variant="info" href={user.url} target="_blank">
+                            <i className="fa fa-link fa-2x"></i>
+                        </Button>}
+                    </ButtonGroup>
                 </Card.Body>
             </Card>
         </Col>))}
