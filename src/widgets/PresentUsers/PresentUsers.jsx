c72f8144a9c935076a7a0f4f2490b10bfa6b6980
Vencislav Atanasov <git@6bez10.info>
2022-05-09 00:34:48 +0300

    Fix present users layout

--- src/widgets/PresentUsers/PresentUsers.jsx
+++ src/widgets/PresentUsers/PresentUsers.jsx
@@ -15,41 +15,45 @@ const PresentUsers = () => {
         pollingInterval: 60000,
     });
 
-    return (<Row className={isSuccess && 'row-cols-1 row-cols-sm-2 row-cols-lg-4 gy-3 mb-3'}>
+    const hasUsers = isSuccess && users.length > 0;
+
+    return (<Row className={'mb-3' + (isSuccess && hasUsers ? ' row-cols-1 row-cols-sm-2 row-cols-lg-4 gy-3' : '')}>
         {isLoading && <Col>
             <LoadingIcon large/>
         </Col>}
         {isError && <Col>Error</Col>}
-        {isSuccess && (users.length === 0 ? (<Col className="text-center no_users">
-                <i className="far fa-frown"/>
-                <h5>{t('views.users.everybodys_gone')}</h5>
-            </Col>
-        ) : users.map(user => <Col key={user.picture}>
-            <Card className="h-100">
-                <Card.Header>
-                    {user.picture &&
-                        <Image fluid src={user.picture.replace('s=128', 's=242')} alt="avatar" className="w-100"/>}
-                </Card.Header>
-                <Card.Body className="text-center">
-                    <h5>
-                        {user.name}
-                        {user.username && <div className="small">{user.username}</div>}
-                    </h5>
-                    <ButtonGroup>
-                        {user.twitter && <Button href={'https://twitter.com/' + user.twitter} target="_blank">
-                            <i className="fab fa-twitter fa-2x"></i>
-                        </Button>}
-                        {user.github &&
-                            <Button variant="default" href={'https://github.com/' + user.github} target="_blank">
-                                <i className="fab fa-github fa-2x"></i>
+        {isSuccess && <>
+            {hasUsers && users.map(user => <Col key={user.picture}>
+                <Card className="h-100">
+                    <Card.Header>
+                        {user.picture &&
+                            <Image fluid src={user.picture.replace('s=128', 's=242')} alt="avatar" className="w-100"/>}
+                    </Card.Header>
+                    <Card.Body className="text-center">
+                        <h5>
+                            {user.name}
+                            {user.username && <div className="small">{user.username}</div>}
+                        </h5>
+                        <ButtonGroup>
+                            {user.twitter && <Button href={'https://twitter.com/' + user.twitter} target="_blank">
+                                <i className="fab fa-twitter fa-2x"></i>
                             </Button>}
-                        {user.url && <Button variant="info" href={user.url} target="_blank">
-                            <i className="fas fa-link fa-2x"></i>
-                        </Button>}
-                    </ButtonGroup>
-                </Card.Body>
-            </Card>
-        </Col>))}
+                            {user.github &&
+                                <Button variant="default" href={'https://github.com/' + user.github} target="_blank">
+                                    <i className="fab fa-github fa-2x"></i>
+                                </Button>}
+                            {user.url && <Button variant="info" href={user.url} target="_blank">
+                                <i className="fas fa-link fa-2x"></i>
+                            </Button>}
+                        </ButtonGroup>
+                    </Card.Body>
+                </Card>
+            </Col>)}
+            {hasUsers || <Col className="text-center no_users">
+                <i className="far fa-frown"/>
+                <h5 className="mt-2">{t('views.users.everybodys_gone')}</h5>
+            </Col>}
+        </>}
     </Row>);
 };
 
