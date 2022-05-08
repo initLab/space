2bbc15942540472e94cb8288a00302e68d7fe655
Vencislav Atanasov <git@6bez10.info>
2022-05-09 02:36:29 +0300

    Show MQTT readings from Redux

--- src/widgets/SensorReading/SensorReading.jsx
+++ src/widgets/SensorReading/SensorReading.jsx
@@ -1,11 +1,23 @@
 import {Card, Col, Container, Row} from "react-bootstrap";
 import PropTypes from "prop-types";
 import './SensorReading.css';
+import {useSelector} from "react-redux";
+import {sensorSelector} from "../../features/sensorSlice";
+import LoadingIcon from "../icons/LoadingIcon";
+
+const units = {
+    'Temperature': ['°C', 1],
+    'Humidity': ['%', 1]
+};
 
 const SensorReading = ({
     label,
     topic,
 }) => {
+    const value = useSelector(sensorSelector(topic));
+    const unit = units.Temperature; // TODO
+    const formattedValue = value?.data && parseFloat(value.data).toFixed(unit[1]) + unit[0];
+
     return (<Col>
         <Card bg="primary" text="white">
             <Card.Body>
@@ -15,7 +27,7 @@ const SensorReading = ({
                             <i className="fas fa-thermometer-half fa-5x" />
                         </Col>
                         <Col xs={9} className="text-end">
-                            <div className="huge">21.9°C</div>
+                            <div className="huge">{formattedValue || <LoadingIcon />}</div>
                             <div>{label}</div>
                         </Col>
                     </Row>
