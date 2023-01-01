add8328634bf236ecf48c9338ede86c7b6f3ba7f
Vencislav Atanasov <git@6bez10.info>
2023-01-01 22:26:04 +0200

    Fix react router base URL

--- src/main.jsx
+++ src/main.jsx
@@ -15,7 +15,7 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <Provider store={store}>
-            <BrowserRouter>
+            <BrowserRouter basename={import.meta.env.BASE_URL}>
                 <App />
             </BrowserRouter>
         </Provider>
