aae3976a14343faa0afc062d286ba5bbe310fc96
Vencislav Atanasov <git@6bez10.info>
2023-01-02 03:26:05 +0200

    i18n in footer

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,19 +1,25 @@
 import {Col, Container, Row} from "react-bootstrap";
 import './Footer.scss';
+import {useTranslation} from "react-i18next";
 
 const Footer = () => {
+    const {t} = useTranslation();
+
     return (<footer>
         <Container>
             <Row>
                 <Col xs={12} sm={6}>
                     <p className="my-4">
-                        init Lab Fauna Frontend is licensed
-                        under <a href="https://github.com/initLab/fauna-frontend/blob/master/LICENSE">MIT License</a>
+                        {t('views.footer.license')
+                            .replace('init Lab Fauna', 'init Lab Fauna Frontend')}{' '}
+                        <a href="https://github.com/initLab/fauna-frontend/blob/master/LICENSE">MIT License</a>
                     </p>
                 </Col>
                 <Col xs={12} sm={6}>
-                    <p className="my-4 right">The source code is available in <a
-                        href="https://github.com/initLab/fauna-frontend/">GitHub</a></p>
+                    <p className="my-4 right">
+                        {t('views.footer.source_code')}{' '}
+                        <a href="https://github.com/initLab/fauna-frontend/">GitHub</a>
+                    </p>
                 </Col>
             </Row>
         </Container>
