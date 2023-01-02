f5fda82dc86832c72c5336783ed9b07f848cb827
Vencislav Atanasov <git@6bez10.info>
2023-01-02 20:12:18 +0200

    Fix present users layout

--- src/widgets/PresentUsersWrapper/PresentUsersWrapper.jsx
+++ src/widgets/PresentUsersWrapper/PresentUsersWrapper.jsx
@@ -6,15 +6,15 @@ const PresentUsersWrapper = () => {
     const { t } = useTranslation();
 
     return (<>
-        <Row as="header">
+        <Row as="header" className="row-cols row-cols-1 row-cols-lg-2 mt-2">
             <Col>
-                <h2 className="my-4">
-                    {t('views.users.whos_in_the_lab')}
-                    <div className="float-end">
-                        <span className="small">
-                            {t('views.users.people_at_about_html.other')}{' '}
-                            <time dateTime="2022-05-07T19:02:46+03:00">19:02</time>
-                        </span>
+                <h2>{t('views.users.whos_in_the_lab')}</h2>
+            </Col>
+            <Col>
+                <h2>
+                    <div className="text-end small text-muted">
+                        {t('views.users.people_at_about_html.other')}{' '}
+                        <time dateTime="2022-05-07T19:02:46+03:00">19:02</time>
                     </div>
                 </h2>
             </Col>
