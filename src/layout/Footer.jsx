46b4d0b5d3161d664d033a07443cebb7344ac1cb
Vencislav Atanasov <git@6bez10.info>
2023-01-02 03:16:18 +0200

    Fix repo and license URLs

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -6,11 +6,14 @@ const Footer = () => {
         <Container>
             <Row>
                 <Col xs={12} sm={6}>
-                    <p className="my-4">init Lab Fauna is licensed under <a href="https://github.com/initLab/fauna/blob/master/LICENSE">MIT License</a></p>
+                    <p className="my-4">
+                        init Lab Fauna Frontend is licensed
+                        under <a href="https://github.com/initLab/fauna-frontend/blob/master/LICENSE">MIT License</a>
+                    </p>
                 </Col>
                 <Col xs={12} sm={6}>
                     <p className="my-4 right">The source code is available in <a
-                        href="https://github.com/initLab/fauna/">GitHub</a></p>
+                        href="https://github.com/initLab/fauna-frontend/">GitHub</a></p>
                 </Col>
             </Row>
         </Container>
