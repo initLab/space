1ffabc4f40c174196a6ea67f1bd1d2b67f3c4183
Vencislav Atanasov <git@6bez10.info>
2023-03-07 00:11:03 +0200

    Hide doors with no actions, show no access message when needed

--- src/pages/Doors.jsx
+++ src/pages/Doors.jsx
@@ -1,13 +1,16 @@
 import { Card, Col, Row } from 'react-bootstrap';
-import React, { useCallback } from 'react';
+import React, { useCallback, useMemo } from 'react';
 import { useDoorActionMutation, useGetDoorsQuery } from '../features/apiSlice.js';
 import LoadingIcon from '../widgets/icons/LoadingIcon.jsx';
 import DoorButton from '../widgets/DoorButton/DoorButton.jsx';
 import { useSelector } from 'react-redux';
 import { doorStatusSelector } from '../features/doorSlice.js';
 import { Navigate } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 
 const Doors = () => {
+    const { t } = useTranslation();
+
     const {
         data: doors,
         error,
@@ -15,6 +18,11 @@ const Doors = () => {
         isSuccess,
         isError,
     } = useGetDoorsQuery();
+
+    const availableDoors = useMemo(
+        () => isSuccess ? doors.filter(door => door.supported_actions.length > 0) : [],
+        [doors, isSuccess],
+    );
 
     const [
         execute,
@@ -45,24 +53,30 @@ const Doors = () => {
         {isLoading && <Col className="text-center">
             <LoadingIcon large />
         </Col>}
-        {isSuccess && doors.map(door => {
-            const doorActions = getDoorActions(door);
+        {isSuccess && <>
+            {availableDoors.length > 0 ? availableDoors.map(door => {
+                const doorActions = getDoorActions(door);
 
-            return (<Col key={door.id}>
+                return (<Col key={door.id}>
+                    <Card>
+                        <Card.Header className="bg-primary text-light text-start">{door.name}</Card.Header>
+                        <Card.Body
+                            className="d-flex flex-column flex-lg-row justify-content-center align-items-center gap-4">
+                            {doorActions.map(action => <DoorButton key={action} action={action} onClick={() => execute({
+                                doorId: door.id,
+                                action,
+                            })} />)}
+
+                            {door.id === monitoredDoor && doorStatus === 'busy' && <LoadingIcon large />}
+                        </Card.Body>
+                    </Card>
+                </Col>);
+            }) : <Col>
                 <Card>
-                    <Card.Header className="bg-primary text-light text-start">{door.name}</Card.Header>
-                    <Card.Body
-                        className="d-flex flex-column flex-lg-row justify-content-center align-items-center gap-4">
-                        {doorActions.map(action => <DoorButton key={action} action={action} onClick={() => execute({
-                            doorId: door.id,
-                            action,
-                        })} />)}
-
-                        {door.id === monitoredDoor && doorStatus === 'busy' && <LoadingIcon large />}
-                    </Card.Body>
+                    <Card.Body className="d-flex flex-column flex-lg-row gap-4">{t('views.doors.no_access')}</Card.Body>
                 </Card>
-            </Col>);
-        })}
+            </Col>}
+        </>}
         {isError && error.status === 401 && <Navigate to="/login" />}
     </Row>);
 };
