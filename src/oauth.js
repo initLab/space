b632b4903a0adeec5b2b551edb7fa3fd4903ad06
Vencislav Atanasov <git@6bez10.info>
2023-03-10 14:52:55 +0200

    Dynamically generate OAuth callback URL and remove VITE_OAUTH_CALLBACK_URL env variable

--- src/oauth.js
+++ src/oauth.js
@@ -4,7 +4,7 @@ const scopes = ['public', 'account_data_read', 'door_control'].join(' ');
 
 const pkce = new PKCE({
     client_id: import.meta.env.VITE_OAUTH_CLIENT_ID,
-    redirect_uri: import.meta.env.VITE_OAUTH_CALLBACK_URL,
+    redirect_uri: window.location.protocol + '//' + window.location.host + import.meta.env.BASE_URL + 'oauth-callback',
     authorization_endpoint: import.meta.env.VITE_BACKEND_URL + 'oauth/authorize',
     token_endpoint: import.meta.env.VITE_BACKEND_URL + 'oauth/token',
     requested_scopes: scopes,
