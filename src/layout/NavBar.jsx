b1d6934086220a12bf678aeb8b14a01e41065026
Vencislav Atanasov <git@6bez10.info>
2023-07-08 15:45:01 +0300

    Show fauna users management only to board members

--- src/layout/NavBar.jsx
+++ src/layout/NavBar.jsx
@@ -13,6 +13,7 @@ import BusyIcon from '../widgets/icons/BusyIcon.jsx';
 import { getToken } from '../authStorage.js';
 import DoorClosedIcon from '../widgets/icons/DoorClosedIcon.jsx';
 import DoorOpenIcon from '../widgets/icons/DoorOpenIcon.jsx';
+import { useGetCurrentUserQuery } from '../features/apiSlice.js';
 
 const NavBar = () => {
     const {t} = useTranslation();
@@ -20,6 +21,13 @@ const NavBar = () => {
     const doorLockStatus = useSelector(doorLockStatusSelector());
     const backendUrl = import.meta.env.VITE_BACKEND_URL;
     const isLoggedIn = getToken() !== null;
+    const {
+        data,
+        isSuccess,
+    } = useGetCurrentUserQuery(undefined, {
+        skip: !isLoggedIn,
+    });
+    const isBoardMember = isSuccess && data.roles.includes('board_member');
 
     return (<Navbar bg="primary" variant="dark" expand="lg" className="py-0">
         <Container>
@@ -57,34 +65,31 @@ const NavBar = () => {
                         <i className="fas fa-chart-line" />{' '}
                         {t('views.navigation.sensors')}
                     </Nav.Link>
-                    {isLoggedIn && <>
-                        <Nav.Link href={backendUrl + 'fauna/users'}>
-                            <i className="fas fa-users" />{' '}
-                            {t('views.navigation.labbers')}
-                        </Nav.Link>
-                        <NavDropdown title={<>
+                    {isBoardMember && <Nav.Link href={backendUrl + 'fauna/users'}>
+                        <i className="fas fa-users" />{' '}
+                        {t('views.navigation.labbers')}
+                    </Nav.Link>}
+                    {isLoggedIn ? <NavDropdown title={<>
                             <i className="fas fa-user" />{' '}
                             {t('views.navigation.account')}
                         </>} className="ms-0 ms-lg-auto">
-                            <NavDropdown.Item href={backendUrl + 'users/edit'}>
-                                {t('views.navigation.view_edit')}
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href={backendUrl + 'user/network_devices'}>
-                                {t('views.navigation.network_devices')}
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href={backendUrl + 'oauth/applications'}>
-                                {t('views.navigation.oauth_application_management')}
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href={backendUrl + 'oauth/authorized_applications'}>
-                                {t('views.navigation.oauth_token_management')}
-                            </NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item as={NavLink} to="/logout">
-                                {t('views.navigation.sign_out')}
-                            </NavDropdown.Item>
-                        </NavDropdown>
-                    </>}
-                    {!isLoggedIn && <Nav.Link as={NavLink} to="/login" className="ms-0 ms-lg-auto">
+                        <NavDropdown.Item href={backendUrl + 'users/edit'}>
+                            {t('views.navigation.view_edit')}
+                        </NavDropdown.Item>
+                        <NavDropdown.Item href={backendUrl + 'user/network_devices'}>
+                            {t('views.navigation.network_devices')}
+                        </NavDropdown.Item>
+                        <NavDropdown.Item href={backendUrl + 'oauth/applications'}>
+                            {t('views.navigation.oauth_application_management')}
+                        </NavDropdown.Item>
+                        <NavDropdown.Item href={backendUrl + 'oauth/authorized_applications'}>
+                            {t('views.navigation.oauth_token_management')}
+                        </NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item as={NavLink} to="/logout">
+                            {t('views.navigation.sign_out')}
+                        </NavDropdown.Item>
+                    </NavDropdown> : <Nav.Link as={NavLink} to="/login" className="ms-0 ms-lg-auto">
                         <i className="fas fa-sign-in" />{' '}
                         {t('views.navigation.sign_in')}
                     </Nav.Link>}
