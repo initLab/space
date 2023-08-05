d675ed9690bbcbc58795c8bb4b946a9054e02365
Vencislav Atanasov <git@6bez10.info>
2023-08-06 01:15:52 +0300

    Add support for VERY old browsers, disabled by default

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,23 @@
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteCompression from 'vite-plugin-compression';
+import legacy from '@vitejs/plugin-legacy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    sourcemap: true,
-  },
-  plugins: [
-    react(),
-    viteCompression(),
-  ],
-  server: {
-    open: true,
-  },
+    build: {
+        sourcemap: true,
+    },
+    plugins: [
+        react(),
+        viteCompression(),
+        ...(process.env.BUILD_LEGACY === 'true' ? [
+            legacy({
+                targets: ['defaults', 'android 2.3'],
+            }),
+        ] : []),
+    ],
+    server: {
+        open: true,
+    },
 });
