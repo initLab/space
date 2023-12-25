3ec42513b5bdf8ba4af3877721e640c5e6635162
Vencislav Atanasov <git@6bez10.info>
2023-12-25 19:57:06 +0200

    Fix date/time formatting if value is invalid

--- src/utils/useDateTimeFormatter.js
+++ src/utils/useDateTimeFormatter.js
@@ -1,5 +1,5 @@
 import { useTranslation } from 'react-i18next';
-import { format, formatDistanceToNow } from 'date-fns';
+import { format, formatDistanceToNow, isValid } from 'date-fns';
 import * as locales from 'date-fns/locale';
 
 export function useDateTimeFormatter() {
@@ -8,10 +8,10 @@ export function useDateTimeFormatter() {
     const locale = lang === 'en' ? 'en-US' : lang;
 
     return {
-        formatDefault: dateTime => format(dateTime, 'dd.MM.yyyy HH:mm:ss'),
-        formatDistanceToNow: dateTime => formatDistanceToNow(dateTime, {
+        formatDefault: dateTime => isValid(dateTime) ? format(dateTime, 'dd.MM.yyyy HH:mm:ss') : '',
+        formatDistanceToNow: dateTime => isValid(dateTime) ? formatDistanceToNow(dateTime, {
             addSuffix: true,
             locale: locales[locale],
-        }),
+        }) : '',
     };
 }
