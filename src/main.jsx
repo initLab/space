afd4b3529c972c8c9a2b27211f69153f80baf1ea
Vencislav Atanasov <git@6bez10.info>
2024-06-09 00:36:31 +0300

    Add config and provider for OIDC

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import { BrowserRouter } from 'react-router-dom';
+import { oidc as oidcConfig } from './config';
+import { AuthProvider } from 'react-oidc-context';
 
 import './i18n';
 import App from './App';
@@ -14,7 +16,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <Provider store={store}>
             <BrowserRouter basename={import.meta.env.BASE_URL}>
-                <App />
+                <AuthProvider {...oidcConfig}>
+                    <App />
+                </AuthProvider>
             </BrowserRouter>
         </Provider>
     </React.StrictMode>,
