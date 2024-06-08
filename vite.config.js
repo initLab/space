eaaa59c9aa120f879b06ad65231e8254c5d32884
Vencislav Atanasov <git@6bez10.info>
2024-06-08 22:30:40 +0300

    Remove legacy build

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteCompression from 'vite-plugin-compression';
-import legacy from '@vitejs/plugin-legacy';
-
-const isLegacyBuild = process.env.BUILD_LEGACY === 'true';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -18,23 +15,9 @@ export default defineConfig({
         'OAUTH_CLIENT_ID',
         'VARIANT',
     ],
-    ...(isLegacyBuild ? {
-        optimizeDeps: {
-            esbuildOptions: {
-                supported: {
-                    bigint: true,
-                },
-            },
-        },
-    } : {}),
     plugins: [
         react(),
         viteCompression(),
-        ...(isLegacyBuild ? [
-            legacy({
-                targets: ['defaults', 'android 2.3'],
-            }),
-        ] : []),
     ],
     server: {
         open: true,
