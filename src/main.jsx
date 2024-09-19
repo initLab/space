63673e32561895db416e596034d53586081b7ee6
Vencislav Atanasov <git@6bez10.info>
2024-09-19 20:25:26 +0300

    Fix imports in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import { BrowserRouter } from 'react-router-dom';
@@ -10,12 +10,12 @@ import App from './App';
 import '@fortawesome/fontawesome-free/css/all.css';
 import 'bootswatch/dist/materia/bootstrap.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
+createRoot(document.getElementById('root')).render(
+    <StrictMode>
         <Provider store={store}>
             <BrowserRouter basename={import.meta.env.BASE_URL}>
                 <App />
             </BrowserRouter>
         </Provider>
-    </React.StrictMode>,
+    </StrictMode>,
 );
