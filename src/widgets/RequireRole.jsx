703dd0c91ce03a702cfbec5daf49769aa0af45d6
Vencislav Atanasov <git@6bez10.info>
2024-09-19 23:05:13 +0300

    Fix RequireRole

--- src/widgets/RequireRole.jsx
+++ src/widgets/RequireRole.jsx
@@ -10,7 +10,7 @@ const RequireRole = ({
     } = useCurrentUser();
 
     const searchRoles = typeof roles === 'string' ? [roles] : Array.from(roles);
-    const isAllowed = (user.roles || []).some(userRole => searchRoles.includes(userRole));
+    const isAllowed = (user?.roles || []).some(userRole => searchRoles.includes(userRole));
 
     return isAllowed ? children : null;
 };
