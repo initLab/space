b97dfd9a313b28b8385d67005446db4c4910937c
Albert Stefanov <aastefanov@outlook.com>
2025-12-22 20:48:16 +0200

    Use portier permissions for everything
    Don't reroute based on hardcoded roles or variants
    This actually checked which design was used before...
    
    + fix the package.json mishap

--- src/pages/ActionLog.tsx
+++ src/pages/ActionLog.tsx
@@ -2,20 +2,14 @@ import {Col, Row} from 'react-bootstrap';
 import {useTranslation} from 'react-i18next';
 import {Navigate} from 'react-router-dom';
 
-import {useActionLog, useCurrentUser} from '../hooks/useEndpoints.ts';
+import {useActionLog} from '../hooks/useEndpoints.ts';
 import LoadingIcon from '../widgets/LoadingIcon.tsx';
 import ErrorMessage from '../widgets/ErrorMessage.tsx';
-import {hasRole} from "../helpers.ts";
 import ActionLogTable from "../widgets/ActionLog/ActionLogTable.tsx";
 
 const ActionLog = () => {
     const {t} = useTranslation();
-    const {data: user} = useCurrentUser();
     const {data: actionLog, error, isLoading} = useActionLog({refreshInterval: 60_000,});
-
-    if (!hasRole(user, 'board_member')) {
-        return <Navigate to="/doors"/>;
-    }
 
     return <>
         <Row>
