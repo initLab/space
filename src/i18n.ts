cd86b52b2be4dce68f8b9e14f7537323b7bf3740
Albert Stefanov <albert@kmail.bg>
2026-04-20 12:42:49 +0300

    lint: make it pass

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,7 +1,5 @@
 import i18n from 'i18next';
-import Backend from 'i18next-http-backend';
 import {initReactI18next} from 'react-i18next';
-import {load} from 'js-yaml';
 
 import resources from 'virtual:i18next-loader';
 
